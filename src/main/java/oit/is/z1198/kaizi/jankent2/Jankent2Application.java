db3829e05276372b0c11d15f417ce14b8542d431
e1b20059 <e1b20059@oit.ac.jp>
2022-10-04 14:41:38 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1198/kaizi/jankent2/Jankent2Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1198.kaizi.jankent2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jankent2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jankent2Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1198/kaizi/jankent2/Jankent2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1198.kaizi.jankent2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jankent2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
